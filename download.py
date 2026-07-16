2798af34f0f5173fc5a922cc545d8eebd0d32bd0
Nguyen Khanh Tai <nguyenkhanhtai2307@gmail.com>
2026-07-16 11:28:42 +0000

    Remove download.py

--- download.py
+++
@@ -1,6 +0,0 @@
-import gdown
-
-file_id = "1dX1bCthvy_9Q_qRS5Qf_17zlsaziYs61"
-
-# The use_cookies parameter is True by default, and gdown looks at ~/.cache/gdown/cookies.txt
-gdown.download(id=file_id, use_cookies=True, quiet=False)
