185df3eb57a19acae8df8f174a7035b1e94f34dd
root <nguyenkhanhtai2307@gmail.com>
2026-07-17 07:11:42 +0000

    Add category classification to the test

--- test_fuzz5.py
+++
@@ -1,9 +0,0 @@
-from rapidfuzz import fuzz
-
-q = "monika grozio salonas"
-t = "o"
-
-print("token_set_ratio:", fuzz.token_set_ratio(q, t))
-print("token_sort_ratio:", fuzz.token_sort_ratio(q, t))
-print("partial_ratio:", fuzz.partial_ratio(q, t))
-
